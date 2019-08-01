07735c474f9f9da7db358fce19001ff8be2383d2
Marius Beckers <marius@beckers.de>
2019-08-01 09:34:21 +0200

    added filter for locality

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Topic;
 use App\Paper;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class TopicController extends Controller
 {
     public function all(Request $request)
     {
+        $postalCode = $request->input('postalCode');
+        $district = $request->input('district');
+
         $paperQuery = \App\Paper::with(['location']);
+
+        if ($postalCode) {
+            $paperQuery->whereHas('location', function (Builder $query) use ($postalCode){
+                $query->where('postal_code', '=', $postalCode);
+            });
+        }
+
+        if ($district) {
+            $paperQuery->whereHas('location', function (Builder $query) use ($district){
+                $query->where('sub_locality', '=', $district);
+            });
+        }
+
 
         return Topic::collection($paperQuery->paginate(100));
     }

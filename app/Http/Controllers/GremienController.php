0f8a70d01f808cba9a758d61f6a4a9dde9efc6fe
Marius Beckers <marius@beckers.de>
2019-08-08 14:57:37 +0200

    changed to respond with all organisations

--- app/Http/Controllers/GremienController.php
+++ app/Http/Controllers/GremienController.php
@@ -8,12 +8,11 @@ class GremienController extends Controller
 {
     public function list()
     {
-        $committees = \App\Http\Resources\Organization::collection(Organization::paginate(100))
+        $committees = \App\Http\Resources\Organization::collection(Organization::all())
             ->toResponse(request())->getData();
 
         return view('committee')->with([
             'committees' => $committees->data,
-            'links' => $committees->links,
         ]);
     }
 }

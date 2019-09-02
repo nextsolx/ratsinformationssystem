fbe2712842caa810a2182812e486ceb854cdaf9d
jovan <jovan@CV-Admins-MacBook-Pro-5.local>
2019-09-02 10:43:38 +0300

    add clickoutside

--- resources/js/components/Ux/Collapse.vue
+++ resources/js/components/Ux/Collapse.vue
@@ -1,6 +1,10 @@
 <script>
+import clickOutSide from '../tools/clickOutSide';
 export default {
     name: 'Collapse',
+    directives: {
+        'outside': clickOutSide
+    },
     props: {
         optionList: {
             type: Object,
@@ -11,39 +15,64 @@ export default {
         return {
             activeFilter: false
         };
+    },
+    computed: {
+        pathname () {
+            return window.location.pathname;
+        },
+        search () {
+            return window.location.search;
+        },
+    },
+    methods: {
+        hide () {
+            this.activeFilter = false;
+        },
+        linkUrl (value) {
+            if (this.search) {
+                return '';
+            }
+            else {
+                return `${window.location.pathname}/?${this.optionList.type}=${value.toLowerCase()}`;
+            }
+        },
     }
 };
 </script>
 
 <template>
-        <div class="ris-filter" :class="{'ris-filter_active': activeFilter}">
-            <button class="ris-filter__subheader ris-subheader" @click="activeFilter = !activeFilter">
-                <span class="ris-i ris-i_filter"/>Filtern
-            </button>
+    <div
+        class="ris-filter"
+        :class="{'ris-filter_active': activeFilter}"
+        v-outside="hide">
+        <button class="ris-filter__subheader ris-subheader" @click="activeFilter = !activeFilter">
+            <span class="ris-i ris-i_filter"/>Filtern
+        </button>
 
-            <div class="ris-filter__content">
-                <!--<div class="ris-filter-buttons ris-filter-buttons__selected"
-                    :class="{'ris-filter-buttons__selected_active' : selectedDistrictList.length }">
-                    <button class="ris-label ris-label_has-border"
-                        v-for="currentDistrictName in selectedDistrictList"
-                        :key="currentDistrictName">
-                        {{ currentDistrictName }}
-                        <span class="ris-i ris-i_close"/>
-                    </button>
-                </div>-->
+        <div class="ris-filter__content">
+<!--            <div class="ris-filter-buttons ris-filter-buttons__selected"-->
+<!--                :class="{'ris-filter-buttons__selected_active' : selectedDistrictList.length }">-->
+<!--                <button class="ris-label ris-label_has-border"-->
+<!--                    v-for="currentDistrictName in selectedDistrictList"-->
+<!--                    :key="currentDistrictName">-->
+<!--                    {{ currentDistrictName }}-->
+<!--                    <span class="ris-i ris-i_close"/>-->
+<!--                </button>-->
+<!--            </div>-->
 
-                <div class="ris-filter-buttons">
-                    <span class="ris-filter-buttons__title">
-                        Nach Bezirken filtern
-                    </span>
-                    <button
-                        class="ris-button ris-button_secondary ris-button_has-shadow"
-                        v-for="district in optionList.data"
-                        :key="district">
-                        {{ district }}
-                    </button>
-                </div>
+            <div class="ris-filter-buttons">
+                <span class="ris-filter-buttons__title">
+                    Nach Bezirken filtern
+                </span>
+                <a
+                    class="ris-button ris-button_secondary ris-button_has-shadow"
+                    v-for="district in optionList.data"
+                    :href="linkUrl(district)"
+                    :key="district">
+                    {{ district }}
+                </a>
             </div>
         </div>
+    </div>
 </template>
 
